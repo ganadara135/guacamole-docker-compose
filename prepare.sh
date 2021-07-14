cd1a9f252361c92fb2f34eedd4820d293c98a0cd
ganadara135 <ganadara135@gmail.com>
2021-07-14 15:04:40 +0900

    first commit

--- prepare.sh
+++ prepare.sh
@@ -7,6 +7,6 @@ chmod -R +x ./init
 docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgres > ./init/initdb.sql
 echo "done"
 echo "Creating SSL certificates"
-openssl req -nodes -newkey rsa:2048 -new -x509 -keyout nginx/ssl/self-ssl.key -out nginx/ssl/self.cert -subj '/C=DE/ST=BY/L=Hintertupfing/O=Dorfwirt/OU=Theke/CN=www.createyourown.domain/emailAddress=docker@createyourown.domain'
+openssl req -nodes -newkey rsa:2048 -new -x509 -keyout nginx/ssl/self-ssl.key -out nginx/ssl/self.cert -subj '/C=KR/ST=None/L=None/O=None/OU=None/CN=www.parongolf.com/emailAddress=ganadara135@gmail.com'
 echo "You can use your own certificates by placing the private key in nginx/ssl/self-ssl.key and the cert in nginx/ssl/self.cert"
 echo "done"
